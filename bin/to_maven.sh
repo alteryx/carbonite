26c9835f0400f0ae268bcb7a85bb34f852745337
Mark Hamstra <markhamstra@gmail.com>
2013-07-22 11:37:55 -0700

    Removed unnecessary loop from build/deploy script

--- bin/to_maven.sh
+++ bin/to_maven.sh
@@ -57,44 +57,42 @@ build() {
 time build
 
 deployment_error=""
-for artifact_id in carbonite; do
-  path_prefix=target/$artifact_id-$version
-  for f in $path_prefix.jar $path_prefix.pom; do
+path_prefix=target/carbonite-$version
+for f in $path_prefix.jar $path_prefix.pom; do
     if [ ! -f "$f" ]; then
-      echo "File $f not found" >&2
-      exit 1
+        echo "File $f not found" >&2
+        exit 1
     fi
-  done
-  set +x
-  mvn_common_args="-Dfile=$path_prefix.jar \
+done
+set +x
+mvn_common_args="-Dfile=$path_prefix.jar \
                    -DpomFile=$path_prefix.pom \
                    -DgroupId=storm \
-                   -DartifactId=$artifact_id \
+                   -DartifactId=carbonite \
                    -Dversion=$version \
                    -DgeneratePom=false \
                    -Dpackaging=jar"
-  mvn_common_args=$( echo $mvn_common_args )
-  set -x
-  case "$action" in
+mvn_common_args=$( echo $mvn_common_args )
+set -x
+case "$action" in
     install)
-      mvn install:install-file $mvn_common_args ;;
+        mvn install:install-file $mvn_common_args ;;
     deploy)
-      set +e  # Ignore failures to deploy
-      mvn deploy:deploy-file \
-        $mvn_common_args \
-        -DrepositoryId=$MAVEN_REPOSITORY_ID \
-        -Durl=$MAVEN_REPOSITORY_URL
-      if [ $? != 0 ]; then
-        deployment_error=1
-      fi
-      set -e
-      ;;
+        set +e  # Ignore failures to deploy
+        mvn deploy:deploy-file \
+            $mvn_common_args \
+            -DrepositoryId=$MAVEN_REPOSITORY_ID \
+            -Durl=$MAVEN_REPOSITORY_URL
+        if [ $? != 0 ]; then
+            deployment_error=1
+        fi
+        set -e
+        ;;
     *)
-      set +x
-      echo "Invalid action: $action" >&2
-      exit 1
-  esac
-done
+        set +x
+        echo "Invalid action: $action" >&2
+        exit 1
+esac
 
 if [ "$deployment_error" ]; then
   echo "Deployment failed" >&2
